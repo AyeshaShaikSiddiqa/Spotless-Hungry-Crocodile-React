03f2e457ed25a00d86bf587c1f389236243ad41d
AyeshaShaikSiddiqa <133034872+AyeshaShaikSiddiqa@users.noreply.github.com>
2024-01-21 14:49:51 +0530

    Commit from TeleportHQ

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom'
+
+import './style.css'
+import NotFound1, { NotFound } from './views/not-found'
+import Home1 from './views/home1'
+import NotFound11 from './views/not-found1'
+import Home from './views/home'
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route component={NotFound1} exact path="/not-found" />
+        <Route component={Home1} exact path="/home1" />
+        <Route component={NotFound11} exact path="/not-found1" />
+        <Route component={Home} exact path="/" />
+        <Route component={NotFound} path="**" />
+        <Redirect to="**" />
+      </Switch>
+    </Router>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
